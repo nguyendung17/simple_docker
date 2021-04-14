0aedc23fbf9792cf81bc960ffa8c33048ff8b1e4
nguyendung <nguyendung17@outlook.com>
2021-04-14 16:50:38 +0700

    Multi Container

--- app/public/index.php
+++ app/public/index.php
@@ -1,3 +1,9 @@
 <?
-echo  "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+session_start();
+$t = isset($_SESSION['t'])?$_SESSION['t']:0;
+$t++;
+echo  "http://$_SERVER[HTTP_HOST]:$_SERVER[SERVER_PORT]$_SERVER[REQUEST_URI]";
+echo "<br>";
+echo $t;
+$_SESSION["t"] = $t;
 ?>
--- docker/work.sh
+++ docker/work.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 role=$1;
 echo $role;
+
+FILE=/var/www/artisan
+
 while [ true ]
-do
-if [ "$role" = "schedule" ]; then
-    php /var/www/artisan schedule:run --verbose --no-interaction &
-elif [ "$role" = "queue" ]; then
-    php /var/www/artisan queue:work --timeout=30
-fi
-  sleep 60
+  do
+  if [ -f "$FILE" ]; then
+  # Take action if $DIR exists. #
+    if [ "$role" = "schedule" ]; then
+        php /var/www/artisan schedule:run --verbose --no-interaction &
+    elif [ "$role" = "queue" ]; then
+        php /var/www/artisan queue:work --timeout=30
+    fi
+    sleep 60
+  fi
 done
+
